755c34d5a483983037c1c12fdbb4225f4f76b5bc
robert <rob_todrcl_01@hotmail.com>
2018-11-25 23:57:21 -0400

    Arreglando tests4

--- features/step_definitions/game_steps.rb
+++ features/step_definitions/game_steps.rb
@@ -1,16 +1,3 @@
-require './lib/jugador.rb'
-$juga1 = Jugador.new('Juan')
-$juga2 = Jugador.new('Pedro')
-
-
-Given("visito la pagina de game") do
-    visit('/game') # Write code here that turns the phrase above into concrete actions
-end
-  
-Then("deberia de ver el mensaje en la partida {string}") do |mensaje|
-    expect(page).to have_content(mensaje) # Write code here that turns the phrase above into concrete actions
-end
-
 
 
 
